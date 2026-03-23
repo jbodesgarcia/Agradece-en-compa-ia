4268f3fa5a17e0f3ccf7255887fca7aea2b3ec6c
jbodesgarcia <joseantoniobodesgarcia.guadalupe@alumnado.fundacionloyola.net>
2026-03-23 16:36:51 +0100

    Create insercion.sql

---
+++ BBDD/insercion.sql
@@ -0,0 +1,7 @@
+INSERT INTO Alumno (nPuesto, nombre, correo, pw, nick, info, imagen) 
+VALUES
+('01', 'alumno1', 'alumno1@mail.com', '1234', 'Jesuita1', 'info1', 'imagen1'),
+('02', 'alumno2', 'alumno2@mail.com', '1234', 'Jesuita2', 'info2', 'imagen2'),
+('03', 'alumno3', 'alumno3@mail.com', '1234', 'Jesuita3', 'info3', 'imagen3'),
+('04', 'alumno4', 'alumno4@mail.com', '1234', 'Jesuita4', 'info4', 'imagen4'),
+('05', 'alumno5', 'alumno5@mail.com', '1234', 'Jesuita5', 'info5', 'imagen5');
