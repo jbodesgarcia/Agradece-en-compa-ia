d78decab422e0244e99827f8805d0ac1073c1d5f
jbodesgarcia <joseantoniobodesgarcia.guadalupe@alumnado.fundacionloyola.net>
2026-03-26 17:04:43 +0100

    Update and rename insercionAlumno.sql to insercionAlumnos.sql

--- BBDD/insercionAlumno.sql
+++
@@ -1,7 +0,0 @@
-INSERT INTO Alumno (nPuesto, nombre, correo, pw, nick, info, imagen) 
-VALUES
-('01', 'alumno1', 'alumno1@gmail.com', '1234', 'Jesuita1', 'info1', 'imagen1'),
-('02', 'alumno2', 'alumno2@gmail.com', '1234', 'Jesuita2', 'info2', 'imagen2'),
-('03', 'alumno3', 'alumno3@gmail.com', '1234', 'Jesuita3', 'info3', 'imagen3'),
-('04', 'alumno4', 'alumno4@gmail.com', '1234', 'Jesuita4', 'info4', 'imagen4'),
-('05', 'alumno5', 'alumno5@gmail.com', '1234', 'Jesuita5', 'info5', 'imagen5');
---
+++ BBDD/insercionAlumnos.sql
@@ -0,0 +1,7 @@
+INSERT INTO alumnos (equipo, nombre, usuario, password, nombreJesuita, infoJesuita,web) 
+VALUES
+('01', 'alumno1', 'usuario1', '1234', 'Jesuita1', 'info1','web1'),
+('02', 'alumno2', 'usuario2', '1234', 'Jesuita2', 'info2','web2'),
+('03', 'alumno3', 'usuario3', '1234', 'Jesuita3', 'info3','web3'),
+('04', 'alumno4', 'usuario4', '1234', 'Jesuita4', 'info4','web4'),
+('05', 'alumno5', 'usuario5', '1234', 'Jesuita5', 'info5','web5');
